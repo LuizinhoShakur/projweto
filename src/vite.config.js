440e7be5f3ae087767553c40f6ed3293a1381a81
LuizinhoShakur <arnoldtrue14@gmail.com>
2023-09-02 14:04:26 -0300

    'projeto'

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/projeweto/",
+  base: "/projweto/",
   plugins: [],
   resolve: {
     /*something*/
